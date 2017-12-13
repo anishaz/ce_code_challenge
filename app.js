a5aad05e3a66d8a04c41aeaa0bade2c08d2cc505
AnishaZ <anishagzacharia@gmail.com>
2017-12-13 14:39:34 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  let greeting = "Hello World";
+  res.render("index", { greeting: greeting });
+})
+
+app.get('/*', function(req, res) {
+  let path = req.url;
+  // Take path, make lowercase, look for all forward slashes, and replace with a space, then take the first letter in the string and make upperCase
+  let greeting = path.toLowerCase().replace(/(\/)+/g, " ").replace(/\b[a-z]/g, function(makeUpper){return makeUpper.toUpperCase();});
+
+  let slashCount = path.match(/(\/)+/g).length;
+  if (slashCount == 1){
+      greeting = ("Hello" + greeting);
+  };
+
+  res.render("index", { greeting: greeting });
+})
+
+app.listen(6789, function() {
+  console.log("Listening to port 6789");
+})
